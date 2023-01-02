5c1021c41151b4ee5e846a6d33d1d95f5518cd20
antoine-dc <antoinedeconto@gmail.com>
2023-01-02 23:28:04 +0100

    TP2 : Add image and color

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import logo from './assets/pokemon.png'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1><img src={logo} alt="logo" /></h1>
     <App />
   </React.StrictMode>,
 )
