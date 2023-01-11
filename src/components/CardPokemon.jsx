c15f09d30257ed524cd6f19e608bf549db0c1f50
antoine-dc <antoinedeconto@gmail.com>
2023-01-12 00:31:20 +0100

    Add TP 4 : Event

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,29 +1,47 @@
+import "./CardPokemon.scss"
+
 export function CardPokemon(props) {
   let poke = props.data
   return (
     <article
       key={props.index}
-      style={{
-        backgroundColor: getColorHexa(poke.apiTypes[0].name),
-        borderColor: getColorHexa(poke.apiTypes[0].name),
+      data-reverse="false"
+      onClick={(e) => {
+        let currentStatus = e.currentTarget.getAttribute("data-reverse")
+        if (currentStatus === "false") {
+          e.currentTarget.setAttribute("data-reverse", "true")
+        } else {
+          e.currentTarget.setAttribute("data-reverse", "false")
+        }
       }}
     >
-      <figure>
-        <picture>
-          <img src={poke.image} alt={poke.name} title={poke.name} />
-        </picture>
-        <figcaption>
-          <span className="types">{poke.apiTypes[0].name}</span>
-          <h2>{poke.name}</h2>
-          <ol>
-            <li>Points de vie : {poke.stats.HP}</li>
-            <li>Attaque : {poke.stats.attack}</li>
-            <li>Défense : {poke.stats.defense}</li>
-            <li>Attaque spécial : {poke.stats.special_attack}</li>
-            <li>Vitesse : {poke.stats.speed}</li>
-          </ol>
-        </figcaption>
-      </figure>
+      <div
+        className="cardInner"
+        style={{
+          backgroundColor: getColorHexa(poke.apiTypes[0].name),
+          borderColor: getColorHexa(poke.apiTypes[0].name),
+        }}
+      >
+        <figure className="cardFront">
+          <picture>
+            <img src={poke.image} alt={poke.name} title={poke.name} />
+          </picture>
+          <figcaption>
+            <span className="types">{poke.apiTypes[0].name}</span>
+            <h2>{poke.name}</h2>
+            <ol>
+              <li>Points de vie : {poke.stats.HP}</li>
+              <li>Attaque : {poke.stats.attack}</li>
+              <li>Défense : {poke.stats.defense}</li>
+              <li>Attaque spécial : {poke.stats.special_attack}</li>
+              <li>Vitesse : {poke.stats.speed}</li>
+            </ol>
+          </figcaption>
+        </figure>
+        <div className="cardBack">
+          <a href="#">Voir plus</a>
+        </div>
+      </div>
     </article>
   )
 }
@@ -32,11 +50,11 @@ function getColorHexa(nameType) {
   let color
   switch (nameType) {
     case "Eau":
-      color = "blue"
+      color = "#5CB2DE"
       break
 
     case "Plante":
-      color = "green"
+      color = "#2FA043"
       break
 
     case "Poison":
@@ -48,7 +66,7 @@ function getColorHexa(nameType) {
       break
 
     case "Feu":
-      color = "orange"
+      color = "#FEA421"
       break
 
     case "Insecte":
